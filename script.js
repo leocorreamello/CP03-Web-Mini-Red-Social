27b5a0615f6265c680296c6116904ef094fe8418
leocorreamello <leo.correamello@gmail.com>
2024-05-22 01:55:49 -0300

    Faltando: varios posts, filtrador de categoria, e outras coisas

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+//Pega o botão de postar e adiciona um evento de click
 document.getElementById('btnPostar').addEventListener('click', function(postFeed) {
+
+    //Mostra o text do post no Feed
     postFeed.preventDefault();
     const postUsuario = document.getElementById('postUsuario').value;
     document.getElementById('feedPost').innerText = postUsuario;
 
-    const categoriaSelecionada = document.querySelector('#categorias').value;
-    document.querySelector('#feedCategoria').innerText = `Categoria: ${categoriaSelecionada}`;
+    //Verifica se o campo de postagem está vazio
+    if(postUsuario === ''){
+        //Mostra um alerta se o campo de text estiver vazio
+        alert('Digite pelo menos um texto para postar!');
+        return;
+    }else{
+        //Mostra a categoria do post no Feed
+        const categoriaSelecionada = document.querySelector('#categorias').value;
+        document.querySelector('#feedCategoria').innerText = `Categoria: ${categoriaSelecionada}`;
 
-    const agora = new Date();
-    const dataFormatada = agora.toLocaleDateString('pt-BR');
-    const horaFormatada = agora.toLocaleTimeString('pt-BR');
-    document.getElementById('feedData').innerText =`Data e Hora: ${dataFormatada + ' - ' + horaFormatada}`;
-}); 
+        //Mostra a data e hora do post no Feed
+        const agora = new Date();
+        const dataFormatada = agora.toLocaleDateString('pt-BR');
+        const horaFormatada = agora.toLocaleTimeString('pt-BR');
+        document.getElementById('feedData').innerText = `Data e Hora: ${dataFormatada + ' - ' + horaFormatada}`;
+    }
+    
+});
