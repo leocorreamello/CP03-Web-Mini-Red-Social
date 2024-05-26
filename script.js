5faaac3f659f61df20819c17aaebfdabc31a382e
leocorreamello <leo.correamello@gmail.com>
2024-05-26 17:13:24 -0300

    Site Finalizado

--- script.js
+++ script.js
@@ -1,111 +1,108 @@
 document.getElementById('btnPostar').addEventListener('click', function(postFeed) {
-    //Previne o comportamento padrão do botão
     postFeed.preventDefault();
     const postUsuario = document.getElementById('postUsuario').value;
-
-    //Verifica se o usuário digitou algo
     if(postUsuario === ''){
         alert('Digite pelo menos um texto para postar!');
         return;
     }else{
-        //Pega a categoria selecionada
         const categoriaSelecionada = document.querySelector('#categorias').value;
         const fotoUm = document.querySelector("#urlFoto1").value;
         const fotoDois = document.querySelector("#urlFoto2").value;
         const fotoTres = document.querySelector("#urlFoto3").value;
         const fotos = [fotoUm, fotoDois, fotoTres];
-
-        //Pega a data e hora atual
         const agora = new Date();
         const dataFormatada = agora.toLocaleDateString('pt-BR');
         const horaFormatada = agora.toLocaleTimeString('pt-BR');
-
-        //Cria o post
         const postDiv = document.createElement('div');
         postDiv.className = 'post';
         postDiv.style.textAlign = 'center';
-
-        //Adiciona o post do usuário
         const postP = document.createElement('p');
         postP.id = 'feedPost';
         postP.style.marginTop = '1px';
         postP.style.marginBottom = '10px';
         postP.innerText = postUsuario;
         postDiv.appendChild(postP);
-
-        //Adiciona a imagem do post
         if (fotos[0] !== '' || fotos[1] !== '' || fotos[2] !== '') {
             const imagemPost = document.createElement('img');
             imagemPost.src = fotos[0];
+            imagemPost.style.borderRadius = '5px';
             let indiceAtual = 0;
             function proximaImagem() {
-                // Incrementa o índice atual
                 indiceAtual++;
                 if (indiceAtual >= fotos.length) {
                     indiceAtual = 0;
                 }
-                // Atualiza a imagem do post
+                imagemPost.src = fotos[indiceAtual];
+            }
+            function imagemAnterior() {
+                indiceAtual--;
+                if (indiceAtual < 0) {
+                    indiceAtual = fotos.length - 1;
+                }
                 imagemPost.src = fotos[indiceAtual];
             }
             imagemPost.style.width = '100%';
             imagemPost.style.height = 'auto';
-            postDiv.appendChild(imagemPost);
+            const contenedor = document.createElement('div');
+            contenedor.style.position = 'relative';
+            postDiv.appendChild(contenedor);
+            contenedor.appendChild(imagemPost);
+            const botaoProxima = document.createElement('button');
+            botaoProxima.innerHTML = `<span class="material-symbols-outlined">chevron_right</span>`;
+            botaoProxima.style.position = 'absolute';
+            botaoProxima.style.right = '0';
+            botaoProxima.style.top = '50%';
+            botaoProxima.style.borderRadius = '5px';
+            botaoProxima.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+            botaoProxima.style.width = '40px';
+            botaoProxima.addEventListener('click', proximaImagem);
+            contenedor.appendChild(botaoProxima);
+            const botaoAnterior = document.createElement('button');
+            botaoAnterior.innerHTML = `<span class="material-symbols-outlined">chevron_left</span>`;
+            botaoAnterior.style.position = 'absolute';
+            botaoAnterior.style.left = '28.5%';
+            botaoAnterior.style.top = '50%';
+            botaoAnterior.style.borderRadius = '5px';
+            botaoAnterior.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+            botaoAnterior.style.width = '40px';
+            botaoAnterior.addEventListener('click', imagemAnterior);
+            contenedor.appendChild(botaoAnterior);
         }
-
-        // Cria o botão
-        const botao = document.createElement('button');
-        botao.innerText = 'Próxima imagem';
-        botao.style.display = 'block'; // Faz o botão ocupar toda a linha
-        botao.style.margin = 'auto'; // Centraliza o botão na linha
-
-        // Adiciona um ouvinte de eventos ao botão para mudar a imagem quando for clicado
-        botao.addEventListener('click', proximaImagem);
-
-        // Adiciona o botão ao postDiv
-        postDiv.appendChild(botao);
-
-        //Adiciona a categoria
         const categoriaP = document.createElement('p');
         categoriaP.id = 'feedCategoria';
         categoriaP.style.marginTop = '20px';
         categoriaP.style.marginBottom = '10px';
         categoriaP.innerText = `Categoria: ${categoriaSelecionada}`;
         postDiv.appendChild(categoriaP);
-
-        //Adiciona a data e hora
         const dataP = document.createElement('p');
         dataP.id = 'feedData';
         dataP.style.marginTop = '20px';
         dataP.style.marginBottom = '20px';
         dataP.innerText = `Data e Hora: ${dataFormatada + ' - ' + horaFormatada}`;
         postDiv.appendChild(dataP);
-
-        // Cria a div que vai conter os botões
         var botoesDiv = document.createElement('div');
         botoesDiv.style.display = 'flex';
         botoesDiv.style.justifyContent = 'center';
         botoesDiv.style.marginTop = '10px';
         botoesDiv.style.marginBottom  = '20px';
-
-        // Cria o botão de apagar
         var apagarBtn = document.createElement('button');
         apagarBtn.innerHTML = `<span class="material-symbols-outlined">delete</span>Apagar`;
         apagarBtn.style.fontSize = '20px';
         apagarBtn.style.marginLeft = '0%';
         apagarBtn.addEventListener('click', function() {
-            postDiv.remove();
+            if (confirm('Tem certeza que deseja apagar este post?')){
+                alert('Post apagado com sucesso!');
+                postDiv.remove();
+            }
         });
         botoesDiv.appendChild(apagarBtn);
-
-        // Cria o botão de editar
         var editarBtn = document.createElement('button');
         editarBtn.innerHTML = `<span class="material-symbols-outlined">edit</span>Editar`;
         editarBtn.style.fontSize = '20px';
         editarBtn.style.marginLeft = '5px';
         editarBtn.addEventListener('click', function() {
-            var novoTexto = prompt('Digite o novo texto do post:');
+            var novoTexto = prompt('Digite o novo texto do post:', postP.innerText);
             if (novoTexto !== null) {
-                // Verifica se o usuário digitou algo
                 if (novoTexto === '') {
                     alert('Digite pelo menos um texto para postar!');
                     return;
@@ -115,26 +112,13 @@ document.getElementById('btnPostar').addEventListener('click', function(postFeed
             }
         });
         botoesDiv.appendChild(editarBtn);
-        
-        // Adiciona a div de botões ao post
         postDiv.appendChild(botoesDiv);
-
-        // Filtrando post apenas de acordo com o select
         document.getElementById('options').addEventListener('change', function() {
             var categoriaSelecionada = this.value;
-        
-            // Obtém todos os posts
             var posts = document.getElementsByClassName('post');
-        
-            // Itera sobre todos os posts
             for (var i = 0; i < posts.length; i++) {
                 var post = posts[i];
-        
-                // Obtém a categoria do post
                 var categoriaPost = post.querySelector('#feedCategoria').innerText.split(': ')[1];
-        
-                // Se a categoria do post não corresponder à categoria selecionada, esconde o post
-                // Caso contrário, mostra o post
                 if (categoriaSelecionada === 'Todos'){
                     post.style.display = 'block';
                 }else if (categoriaPost !== categoriaSelecionada){
@@ -144,14 +128,7 @@ document.getElementById('btnPostar').addEventListener('click', function(postFeed
                 }
             }
         });
-
-        // Cria o carousel
-        
-
-        //Adiciona o post criado acima ao feed
         document.querySelector('.posts').appendChild(postDiv);
-
-        // Limpa os inputs
         document.getElementById('postUsuario').value = '';
         document.getElementById('categorias').value = 'Todos';
         document.getElementById('urlFoto1').value = '';
